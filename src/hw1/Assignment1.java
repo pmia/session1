11e22ed6ef5afb89cfe685a7e8c4a008413f6583
pmia <parvesmia91@gmail.com>
2023-12-29 19:41:05 -0500

    commited

---
+++ src/hw1/Assignment1.java
@@ -0,0 +1,12 @@
+//Assignment 1:
+//1. Set up a project in Eclipse and print out “Hello, my name is ___” in the console
+//2. Create a *.java file using notepad of the same program (part 1) and run it using CMD
+
+package hw1;
+  public class Assignment1 {
+	public static void main(String[] args) {
+	
+		System.out.println("Hello, My name is PM");
+	}
+}
+ 
